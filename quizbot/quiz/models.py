d210e10b829e70092c1aeb6cd3f39495c3e6a4ae
amixyas <i.amirat@esi-sba.dz>
2022-11-20 12:00:47 +0100

    add answer model

--- quizbot/quiz/models.py
+++ quizbot/quiz/models.py
@@ -22,3 +22,14 @@ class Question(models.Model):
     def __str__(self):
         return self.title
 
+class Answer(models.Model):
+
+    question = models.ForeignKey(Question, related_name='answer', verbose_name=_("Question"), on_delete=models.CASCADE)
+    answer = models.CharField(_("Answer"), max_length=255)
+    is_correct = models.BooleanField(_("Correct Answer"), default=False)
+    is_active = models.BooleanField(_("Is Active"), default=True)
+    created_at = models.DateTimeField(_("Created"), auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated"), auto_now=True, auto_now_add=False)
+    
+    def __str__(self):
+        return self.answer
