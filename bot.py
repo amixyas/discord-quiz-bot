0248dbbc6f6f28d9e42486d7402f786b87b49307
amixyas <i.amirat@esi-sba.dz>
2022-11-20 11:24:26 +0100

    test first event

--- bot.py
+++ bot.py
@@ -4,4 +4,13 @@ import discord
 client = discord.Client()
 
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('hello, I am a bot!')
+        
+        
 client.run('MTA0MzgyOTI0ODIxMDc4NDI5Ng.GrTuUm.X3rXFXazpiEkT15KfCf17YpNJgC4Mfq1qbABhg')
