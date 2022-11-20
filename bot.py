9d1563555bc90318847b6e3796db6c1777ed4ad6
amixyas <i.amirat@esi-sba.dz>
2022-11-20 11:40:37 +0100

    add on_ready event

--- bot.py
+++ bot.py
@@ -1,7 +1,15 @@
 import discord
+from discord.ext import commands
 
+client = discord.Client(intents=discord.Intents.default())
 
-client = discord.Client()
+TOKEN = 'MTA0MzgyOTI0ODIxMDc4NDI5Ng.Gh9DsP.MvHeKHBf1L0ZqPrasenxr86KaljQpjJjNGnlLU'
+#client = commands.Bot(command_prefix='!', intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print('Connected to bot: {}'.format(client.user.name))
+    print('Bot ID: {}'.format(client.user.id))
 
 
 @client.event
@@ -9,8 +17,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
+    if message.content.startswith('$hello'):
         await message.channel.send('hello, I am a bot!')
         
-        
-client.run('MTA0MzgyOTI0ODIxMDc4NDI5Ng.GrTuUm.X3rXFXazpiEkT15KfCf17YpNJgC4Mfq1qbABhg')
+
+""" 
+@client.command()
+async def helloworld(ctx):
+    await ctx.send('Hello World!')
+ """
+
+
+client.run(TOKEN)
